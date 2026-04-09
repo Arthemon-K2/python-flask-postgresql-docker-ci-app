74548307943995bf41f2efab50a7e2e57ef06dc2
Arthemon-K2 <zsinka.zolan@gmail.com>
2026-04-09 18:00:41 +0200

    Enhance Docker and CI setup with health checks, environment variables, and Nginx configuration

--- app/main.py
+++ app/main.py
@@ -14,17 +14,31 @@ def get_db_connection():
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Welcome to the Flask app! /n"
-    "This app connects to a PostgreSQL database and retrieves data from the 'users' table."})
+    return jsonify({"message": "Welcome to the Flask app! \n"
+                               "This app connects to a PostgreSQL database and retrieves data from the 'users' table."})
 
-@app.route('/health')
+@app.route("/health")
 def health():
     try:
         conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT 1;")
+        cur.fetchone()
+        cur.close()
         conn.close()
-        return jsonify({"status": "ok"})
-    except Exception as e:
-        return jsonify({"status": "error", "details": str(e)}), 500
+
+        return jsonify({
+            "status": "ok",
+            "app": "running",
+            "database": "connected"
+        }), 200
+    except Exception as exc:
+        return jsonify({
+            "status": "error",
+            "app": "running",
+            "database": "disconnected",
+            "details": str(exc)
+        }), 500
     
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
